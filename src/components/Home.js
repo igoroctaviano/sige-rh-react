a97bb04cf6c693ec9eaf9997f67af5ba9b0975e8
igoroctaviano <igoroctaviano@gmail.com>
2017-11-08 12:32:51 -0200

    Init project

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Sistemas Integrados de Gestão Empresarial</h1>
+          <h2>Recursos Humanos</h2>
+        </header>
+        <p>
+          Testando! =)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;

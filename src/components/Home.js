4ab18733ff85e627cd2656cd1492a699b0aebdae
igoroctaviano <igoroctaviano@gmail.com>
2017-11-08 16:06:13 -0200

    Add initial page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { scaleRotate as Menu } from "react-burger-menu";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: {
+        items: [
+          <a key="0" href="">
+            <i className="fa fa-fw fa-star-o" />
+            <span>Talentos</span>
+          </a>,
+          <a key="1" href="">
+            <i className="fa fa-fw fa-bell-o" />
+            <span>Planos Contratuais</span>
+          </a>,
+          <a key="2" href="">
+            <i className="fa fa-fw fa-bar-chart-o" />
+            <span>Análise</span>
+          </a>
+        ]
+      }
+    };
+  }
+
   render() {
     return (
-      <div>
-        <header>
-          <h1>Sistemas Integrados de Gestão Empresarial</h1>
-          <h2>Recursos Humanos</h2>
-        </header>
-        <p>
-          Testando! =)
-        </p>
+      <div id="outer-container" style={{ height: "100%" }}>
+        <Menu
+          pageWrapId={"page-wrap"}
+          outerContainerId={"outer-container"}
+          left
+        >
+          {this.state.menu.items}
+        </Menu>
+
+        <main id="page-wrap">
+          <h1>
+            <a href="https://github.com/igoroctaviano/sige-rh">SIGE RH</a>
+          </h1>
+          <h2 className="description">
+            Integração de Recursos Humanos como trabalho prático de Sistemas
+            Integrados de Gestão Empresarial
+          </h2>
+          <nav>
+            <a className="special-btn">API</a>
+            <a className="special-btn">Respositório</a>
+          </nav>
+        </main>
       </div>
     );
   }
